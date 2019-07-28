9cad11673e93cc4766d4395ec8ebbc714a9043e0
K <kamiyaowl@gmail.com>
2019-07-29 01:03:25 +0900

    定義を一新。DHTの読み込みを途中まで実装(Vecの扱いについては別途検討)

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ use std::fs::File;
 use std::io::prelude::*;
 
 struct JpegImage {
+    // SOI
+    read_soi        : bool,
+    // EOI
+    read_eoi        : bool,
     // DQT
-    quantize_table  : [[u16; 64]; 4],
+    quantize_table  : [Option<[u16; 64]>; 4], // qn
     // SOF
     sof_marker      : Option<u16>,
-    height          : Option<usize>,
-    width           : Option<usize>,
-    components      : Option<usize>,
-    component_table : [Component; 4],
+    height          : Option<usize>,  // y
+    width           : Option<usize>,  // x
+    components      : Option<usize>,  // Nf
+    component_table : [Option<Component>; 4], // Tqn
+    // DHT
+    huffman_table  : [Option<HuffmanTableInfo>; 4],
 }
 
 #[derive(Copy, Clone)]
@@ -24,6 +30,13 @@ struct Component {
     h_factor: u8,
     w_factor: u8,
     qt_index: u8,
+}
+
+struct HuffmanTableInfo {
+    is_dc            : bool,
+    id               : usize,    // 0 ~ 3
+    length           : [u8; 16],
+    detifnitions     : Vec<u8>, // DC: { databit }, AC: {upper: runlength, lower: databit} 
 }
 
 struct Image {
@@ -68,35 +81,31 @@ impl Image {
     }
     fn from_jpeg(r: &mut BinaryReader) -> Result<Image, String> {
         let mut dst = JpegImage {
-            quantize_table: [[0; 64]; 4],
+            read_soi: false,
+            read_eoi: false,
+            quantize_table: [None; 4],
             width: None,
             height: None,
             sof_marker: None,
             components: None,
-            component_table: [Component{
-                id: ComponentIdentifier::Unknown,
-                h_factor: 0,
-                w_factor: 0,
-                qt_index: 0,
-            }; 4],
+            component_table: [None; 4],
+            huffman_table: [None; 4],
         };
-        let mut is_soi = false; // start of imageがあったか
-        let mut is_eoi = false; // end of imageがあったか
 
-        while !is_eoi {
-            let marker = r.read_u16_big_endian();
-            is_eoi = match marker {
+        loop {
+            let marker   = r.read_u16_big_endian();
+            let is_abort = match marker {
                 // SOI
                 Some(0xffd8) => {
-                    is_soi = true;
+                    dst.read_soi = true;
                     println!("[SOI] ffd8");
                     false
                 },
                 // EOI
                 Some(0xffd9) => {
-                    is_eoi = true;
+                    dst.read_eoi = true;
                     println!("[EOI] ffd9");
-                    true
+                    false
                 },
                 // DQT
                 Some(0xffdb) => {
@@ -106,25 +115,54 @@ impl Image {
                     let table_index = (identifier & 0x03) as usize;
                     println!("[DQT] ffdb len:{} accuracy:{} index:{}", len, accuracy_byte, table_index);
                     // 順番に読み出して量子化テーブルに格納
+                    let mut table = [0; 64];
                     let entry_count = (len / accuracy_byte) as usize;
                     assert_eq!(entry_count, 64);
+                    
                     for i in 0..entry_count {
                         let v = match accuracy_byte {
                             1 => r.read_u8().unwrap() as u16,
                             2 => r.read_u16_big_endian().unwrap(),
                             _ => panic!("invalid accuracy in DQT"),
                         };
-                        dst.quantize_table[table_index][i] = v;
+                        table[i] = v;
                     }
+                    assert!(table_index < dst.quantize_table.len());
+                    dst.quantize_table[table_index] = Some(table);
                     false
                 },
                 // DHT
-                // Some(0xffc2) => {
-                // },
+                Some(0xffc4) => {
+                    let len = r.read_u16_big_endian().unwrap() - 3; 
+                    println!("[DHT] ffc4 len:{}", len);
+                    let mut info = HuffmanTableInfo {
+                        is_dc        : false,
+                        id           : 0,
+                        length       : [0; 16],
+                        detifnitions : Vec::new(),
+                    };
+                    let class_info = r.read_u8().unwrap();
+                    info.is_dc = if (class_info >> 4) == 0 { true } else { false };
+                    info.id    = (class_info & 0xf) as usize;
+                    // DCにはテーブルビット配分数は割り当てない
+                    let mut length_sum = 0;
+                    for i in 0..16 {
+                        info.length[i] = r.read_u8().unwrap();
+                        length_sum += info.length[i];
+                    }
+                    for i in 0..length_sum {
+                        let data = r.read_u8().unwrap();
+                        info.detifnitions.push(data);
+                    }
+                    println!("[DHT] id:{} is_dc:{}", info.id, info.is_dc);
+                    assert!(info.id < 4);
+                    dst.huffman_table[info.id] = Some(info);
+                    false
+                },
                 // SOF
                 Some(x) if ((0xffc0 <= x) && (x <= 0xffcf) && (x != 0xffc4)) => {
                     dst.sof_marker = Some(x); // ffc0=baseline, ffc2=progressive
-                    let len = r.read_u16_big_endian().unwrap() - 8; // field length分を引く
+                    let len = r.read_u16_big_endian().unwrap() - 8; // ヘッダ部分を除く
                     let precision  = r.read_u8().unwrap() / 8;
                     dst.height     = Some(r.read_u16_big_endian().unwrap() as usize);
                     dst.width      = Some(r.read_u16_big_endian().unwrap() as usize);
@@ -133,7 +171,13 @@ impl Image {
                     // 成分を順番に読み出す
                     assert_eq!(len as usize, 3 * dst.components.unwrap()); // 1Componentの長さは8byteのはず
                     for i in 0..dst.components.unwrap() {
-                        dst.component_table[i].id = match r.read_u8() {
+                        let mut comp = Component {
+                            id: ComponentIdentifier::Unknown,
+                            w_factor: 0,
+                            h_factor: 0,
+                            qt_index: 0,
+                        };
+                        comp.id = match r.read_u8() {
                             Some(1) => ComponentIdentifier::Y,
                             Some(2) => ComponentIdentifier::Cb,
                             Some(3) => ComponentIdentifier::Cr,
@@ -142,10 +186,11 @@ impl Image {
                             _ => panic!("invalid component id"),
                         };
                         let factor: u8 = r.read_u8().unwrap();
-                        dst.component_table[i].w_factor = factor >> 4;
-                        dst.component_table[i].h_factor = factor & 0xf;
-                        dst.component_table[i].qt_index = r.read_u8().unwrap();
-                        println!("component[{}] = w:{} h:{} qt#:{}", i, dst.component_table[i].w_factor, dst.component_table[i].h_factor, dst.component_table[i].qt_index);
+                        comp.w_factor = factor >> 4;
+                        comp.h_factor = factor & 0xf;
+                        comp.qt_index = r.read_u8().unwrap();
+                        println!("[SOF] component[{}] = w:{} h:{} qt#:{}", i, comp.w_factor, comp.h_factor, comp.qt_index);
+                        dst.component_table[i] = Some(comp);
                     }
                     false
                 }
@@ -162,11 +207,12 @@ impl Image {
                     panic!("unexpected eof");
                 },
             };
-            if !is_soi {
-                panic!("bad format(marker 'SOI' not found)");
+            // 終了セグメントを読んだら終わり(is_abortで止まった場合、read_eoiがない)
+            if dst.read_eoi || is_abort {
+                break;
             }
         }
-
+        // 後処理
         let dst = Image::new(1 ,1, 1);
         Ok(dst)
     }
@@ -201,7 +247,10 @@ trait BinaryReader {
 impl BinaryReader for BufReader<File> {
     fn read_raw(&mut self, buf: &mut [u8]) -> usize {
         match self.read(buf) {
-            Ok(n) => n,
+            Ok(n) => {
+                println!("[BinaryReader] read(len={}) : {:02x?}", buf.len(), buf);
+                n
+            },
             Err(_) => 0,
         }
     }    
