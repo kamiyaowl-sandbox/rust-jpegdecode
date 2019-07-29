649dd7be8f4b5bb81fb47b03826d2af5a79c490b
K <kamiyaowl@gmail.com>
2019-07-29 22:43:43 +0900

    JpegImageにstackの内容はすべてもたせるように修正

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,15 @@ struct JpegImage {
     // EOI
     read_eoi        : bool,
     // DQT
-    quantize_table  : [Option<[u16; 64]>; 4], // qn
+    quantize_table  : [[u16; 64]; 4], // qn
     // SOF
-    sof_marker      : Option<u16>,
-    height          : Option<usize>,  // y
-    width           : Option<usize>,  // x
-    components      : Option<usize>,  // Nf
-    component_table : [Option<Component>; 4], // Tqn
+    sof_marker      : u16,
+    height          : usize,  // y
+    width           : usize,  // x
+    components      : usize,  // Nf
+    component_table : [Component; 4], // Tqn
     // DHT
-    huffman_table  : [Option<HuffmanTableInfo>; 4],
+    huffman_table  : [HuffmanTableInfo; 4],
 }
 
 #[derive(Copy, Clone)]
@@ -83,13 +83,44 @@ impl Image {
         let mut dst = JpegImage {
             read_soi: false,
             read_eoi: false,
-            quantize_table: [None; 4],
-            width: None,
-            height: None,
-            sof_marker: None,
-            components: None,
-            component_table: [None; 4],
-            huffman_table: [None; 4],
+            quantize_table: [[0; 64]; 4],
+            width: 0,
+            height: 0,
+            sof_marker: 0,
+            components: 0,
+            component_table: [Component{
+                id: ComponentIdentifier::Unknown,
+                h_factor: 0,
+                w_factor: 0,
+                qt_index: 0,
+            }; 4],
+            // TODO: vecをやめる
+            huffman_table: [
+                HuffmanTableInfo{
+                    is_dc: false,
+                    id: 0,
+                    length: [0; 16],
+                    detifnitions: Vec::new(),
+                },
+                HuffmanTableInfo{
+                    is_dc: false,
+                    id: 0,
+                    length: [0; 16],
+                    detifnitions: Vec::new(),
+                },
+                HuffmanTableInfo{
+                    is_dc: false,
+                    id: 0,
+                    length: [0; 16],
+                    detifnitions: Vec::new(),
+                },
+                HuffmanTableInfo{
+                    is_dc: false,
+                    id: 0,
+                    length: [0; 16],
+                    detifnitions: Vec::new(),
+                },
+            ],
         };
 
         loop {
@@ -115,7 +146,7 @@ impl Image {
                     let table_index = (identifier & 0x03) as usize;
                     println!("[DQT] ffdb len:{} accuracy:{} index:{}", len, accuracy_byte, table_index);
                     // 順番に読み出して量子化テーブルに格納
-                    let mut table = [0; 64];
+                    let mut table = &mut dst.quantize_table[table_index];
                     let entry_count = (len / accuracy_byte) as usize;
                     assert_eq!(entry_count, 64);
                     
@@ -128,24 +159,21 @@ impl Image {
                         table[i] = v;
                     }
                     assert!(table_index < dst.quantize_table.len());
-                    dst.quantize_table[table_index] = Some(table);
                     false
                 },
                 // DHT
                 Some(0xffc4) => {
                     let len = r.read_u16_big_endian().unwrap() - 3; 
                     println!("[DHT] ffc4 len:{}", len);
-                    let mut info = HuffmanTableInfo {
-                        is_dc        : false,
-                        id           : 0,
-                        length       : [0; 16],
-                        detifnitions : Vec::new(),
-                    };
                     let class_info = r.read_u8().unwrap();
-                    info.is_dc = if (class_info >> 4) == 0 { true } else { false };
-                    info.id    = (class_info & 0xf) as usize;
-                    // DCにはテーブルビット配分数は割り当てない
+                    let is_dc = if (class_info >> 4) == 0 { true } else { false };
+                    let id    = (class_info & 0xf) as usize;
+                    assert!(id < 4);
+                    let mut info = &mut dst.huffman_table[id];
                     let mut length_sum = 0;
+
+                    info.is_dc = is_dc;
+                    info.id    = id;
                     for i in 0..16 {
                         info.length[i] = r.read_u8().unwrap();
                         length_sum += info.length[i];
@@ -155,28 +183,21 @@ impl Image {
                         info.detifnitions.push(data);
                     }
                     println!("[DHT] id:{} is_dc:{}", info.id, info.is_dc);
-                    assert!(info.id < 4);
-                    dst.huffman_table[info.id] = Some(info);
                     false
                 },
                 // SOF
                 Some(x) if ((0xffc0 <= x) && (x <= 0xffcf) && (x != 0xffc4)) => {
-                    dst.sof_marker = Some(x); // ffc0=baseline, ffc2=progressive
+                    dst.sof_marker = x; // ffc0=baseline, ffc2=progressive
                     let len = r.read_u16_big_endian().unwrap() - 8; // ヘッダ部分を除く
                     let precision  = r.read_u8().unwrap() / 8;
-                    dst.height     = Some(r.read_u16_big_endian().unwrap() as usize);
-                    dst.width      = Some(r.read_u16_big_endian().unwrap() as usize);
-                    dst.components = Some(r.read_u8().unwrap() as usize);
-                    println!("[SOF] marker:{:x} len:{} width:{} height:{} components:{}", x, len, dst.width.unwrap(), dst.height.unwrap(), dst.components.unwrap());
+                    dst.height     = r.read_u16_big_endian().unwrap() as usize;
+                    dst.width      = r.read_u16_big_endian().unwrap() as usize;
+                    dst.components = r.read_u8().unwrap() as usize;
+                    println!("[SOF] marker:{:x} len:{} width:{} height:{} components:{}", x, len, dst.width, dst.height, dst.components);
                     // 成分を順番に読み出す
-                    assert_eq!(len as usize, 3 * dst.components.unwrap()); // 1Componentの長さは8byteのはず
-                    for i in 0..dst.components.unwrap() {
-                        let mut comp = Component {
-                            id: ComponentIdentifier::Unknown,
-                            w_factor: 0,
-                            h_factor: 0,
-                            qt_index: 0,
-                        };
+                    assert_eq!(len as usize, 3 * dst.components); // 1Componentの長さは8byteのはず
+                    for i in 0..dst.components {
+                        let mut comp = &mut dst.component_table[i];
                         comp.id = match r.read_u8() {
                             Some(1) => ComponentIdentifier::Y,
                             Some(2) => ComponentIdentifier::Cb,
@@ -190,10 +211,9 @@ impl Image {
                         comp.h_factor = factor & 0xf;
                         comp.qt_index = r.read_u8().unwrap();
                         println!("[SOF] component[{}] = w:{} h:{} qt#:{}", i, comp.w_factor, comp.h_factor, comp.qt_index);
-                        dst.component_table[i] = Some(comp);
                     }
                     false
-                }
+                },
                 Some(x) => {
                     let len = r.read_u16_big_endian().unwrap() - 2; // field length分を引く
                     println!("not implemented marker:{:x} len:{}", x, len);
@@ -248,7 +268,7 @@ impl BinaryReader for BufReader<File> {
     fn read_raw(&mut self, buf: &mut [u8]) -> usize {
         match self.read(buf) {
             Ok(n) => {
-                println!("[BinaryReader] read(len={}) : {:02x?}", buf.len(), buf);
+                // println!("[BinaryReader] read(len={}) : {:02x?}", buf.len(), buf);
                 n
             },
             Err(_) => 0,
